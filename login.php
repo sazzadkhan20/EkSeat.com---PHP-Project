03fad4a1205ba33e45ee7db20a23d29e0d252aad
sazzadkhan20 <sazzad53913@gmail.com>
2025-09-15 13:45:57 +0600

    Auto Error Generate

--- login.php
+++ login.php
@@ -10,21 +10,28 @@
     {
         $query = "SELECT * FROM userinfo WHERE uEmail='$email'";
         $query_run = mysqli_query($conn, $query);
-        $row = mysqli_fetch_array($query_run, MYSQLI_ASSOC);
-        if($row >= 1)
-        {
-            if($row['uPassword'] == $password)
+        if ($row = mysqli_fetch_array($query_run, MYSQLI_ASSOC))  
+       {
+            if ($row['uPassword'] === $password)  
+            {
                 header("Location: home.html");
+                exit();
+            }
             else
             {
+                // Redirect back with error
                 echo "Invalid Password";
-                //header("Location: signIn.html");
+                //header("Location: signIn.html?error=InvalidPassword");
+                exit();
             }
         }
         else
         {
+            // Redirect back with error
             echo "Invalid Email";
+            //header("Location: signIn.html?error=InvalidEmail");
+            exit();
         }
-        $conn->close();
-  }
+    }
+    $conn->close();
 ?>
