9abe7bc8381eeaae52c2deea98b8ebe85f0e7ddc
sazzadkhan20 <sazzad53913@gmail.com>
2025-09-27 00:55:34 +0600

    Edit JS

--- View/signUp.php
+++ View/signUp.php
@@ -4,21 +4,20 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- <script src="https://smtpjs.com/v3/smtp.js"></script>
-    <script src="../Controller/emailOTP.js"></script> -->
     <link rel="stylesheet" href="style.css" />
     <title>Sign Up</title>
   </head>
   <body>
+    <form action="verifyOtp.php" method="post">
     <section id="signup">
       <img src="Resources/logo2.jpg" alt="Logo" class="box-logo" />
       <!-- Email input -->
       <input type="text" id="email" placeholder="Enter your email/phone" class="input-box" />
       <p id="errorMessage_SignUp" style="color: red; display: none;"></p>
       <!-- Verify button -->
-      <button class="btn" type="button" onclick="SentOTP()">Send OTP</button>
+      <button class="btn" type="submit">Send OTP</button>
     </section>
-
+    </form>
    <?php include 'footer.html'; ?>
   </body>
 </html>
