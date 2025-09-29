bc5cf5c9c24b3e57dcd97ba40a33de064641a149
sazzadkhan20 <sazzad53913@gmail.com>
2025-09-30 01:05:23 +0600

    Authentication Edit

--- Model/queryExecution.php
+++ Model/queryExecution.php
@@ -33,9 +33,6 @@
         
     function sendOTP()
     {
-        //Import PHPMailer classes into the global namespace
-        //These must be at the top of your script, not inside a function
-
         //Load Composer's autoloader (created by composer, not included with PHPMailer)
         require 'PHPMailer/Exception.php';
         require 'PHPMailer/PHPMailer.php';
